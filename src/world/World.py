062cc7016cdf8b66c2bec2033066e17de9c1e1ca
Guilhermelaivi <guilhermelaivi@gmail.com>
2022-12-05 21:13:04 -0300

    Instantiated player and implemented color logic

--- src/world/World.py
+++ src/world/World.py
@@ -4,14 +4,30 @@ from Pokemao import *
 # from .Pokemao import definitionPokemao 
 import os
 
+class colors:
+    RED = "\033[1;31m"
+    BLUE = "\033[1;96m"
+    GREEN = "\033[1;32m"
+    WHITE = "\033[1;0m"
+def colorPokemao(namePokemao):
+    if namePokemao == PokemaoNames.CHARMAODER.value: 
+        return colors.RED 
+    if namePokemao == PokemaoNames.MAOJADINHA.value:
+        return colors.BLUE
+    if namePokemao == PokemaoNames.MAOSAURO.value:
+        return colors.GREEN
+
+
 print("------------ Seja bem-vindo ao mundo pokemão, se prepare pois você encontrará bastante aventura! ------------ \nBatalhe com bravura!\n")
 
-
-player = input("Defina seu nome:\n")
+playerName = input("Defina seu nome:\n")
 os.system("cls||clear")
-
-print("Seu nome é:",player)
 print("Agora você começará sua jornada no mundo pokemão.\nNote que você já começa com um pokemão e poderá capturar mais ao longo de sua jornada!")
-
 initialPokemao = Pokemao.definitionPokemao()
-print(initialPokemao.name)
+player = Player(playerName, initialPokemao)
+colorInitialPokemao = colorPokemao(initialPokemao.name.value)
+print(f"Mestre pokemão {player.name}, seu pokemão inicial é: " + f"{colorInitialPokemao} {initialPokemao.name.value}")
+
+
+
+
