86b0b9e0f8209b2d7ca2bfaa37692d904404c0ea
Guilhermelaivi <guilhermelaivi@gmail.com>
2022-12-07 17:45:22 -0300

    change attribute on colorPokemao

--- src/world/World.py
+++ src/world/World.py
@@ -1,17 +1,20 @@
 from Player import *
 from Stadium import *
-from Pokemao import * 
-# from .Pokemao import definitionPokemao 
+from Pokemao import *
+# from .Pokemao import definitionPokemao
 import os
+
 
 class colors:
     RED = "\033[1;31m"
     BLUE = "\033[1;96m"
     GREEN = "\033[1;32m"
     WHITE = "\033[1;0m"
+
+
 def colorPokemao(namePokemao):
-    if namePokemao == PokemaoNames.CHARMAODER.value: 
-        return colors.RED 
+    if namePokemao == PokemaoNames.CHARMAODER.value:
+        return colors.RED
     if namePokemao == PokemaoNames.MAOJADINHA.value:
         return colors.BLUE
     if namePokemao == PokemaoNames.MAOSAURO.value:
@@ -25,9 +28,8 @@ os.system("cls||clear")
 print("Agora você começará sua jornada no mundo pokemão.\nNote que você já começa com um pokemão e poderá capturar mais ao longo de sua jornada!")
 initialPokemao = Pokemao.definitionPokemao()
 player = Player(playerName, initialPokemao)
-colorInitialPokemao = colorPokemao(initialPokemao.name.value)
-print(f"Mestre pokemão {player.name}, seu pokemão inicial é: " + f"{colorInitialPokemao} {initialPokemao.name.value}")
-
-
+colorInitialPokemao = colorPokemao(player.pokemaos.name.value)
+print(f"Mestre pokemão {player.name}, seu pokemão inicial é: " +
+      f"{colorInitialPokemao} {player.pokemaos.name.value}")
 
 
