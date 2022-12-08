3710aae3bdc1d0779870da734eb847156d618669
Guilhermelaivi <guilhermelaivi@gmail.com>
2022-12-08 20:15:20 -0300

    validation Pokemao quantity max

--- src/world/Player.py
+++ src/world/Player.py
@@ -1,6 +1,16 @@
 class Player:
     age = 31 
+    pokemaos = []
     def __init__(self,name,pokemaos):
         self.name = name
-        self.pokemaos = pokemaos
+        self.pokemaos.append(pokemaos) 
 
+    def insertPokemao(self,pokemao):
+        print(len(self.pokemaos))
+        if len(self.pokemaos) == 3:  
+            print("Você alcançou o limite máximo de 3 Pokemãos!") 
+            return
+        self.pokemaos.append(pokemao)
+
+
+       
--- src/world/World.py
+++ src/world/World.py
@@ -10,14 +10,7 @@ msgInitialWorld()
 startHistory(Pokemao,Player,playerName,startPokemaos)
 
 
-# Pokemao1 = Pokemao(Types.FIRE,PokemaoNames.CHARMAODER,0,0)
-# Pokemao2 = Pokemao(Types.WATER,PokemaoNames.MAOJADINHA,0,0)
-# Pokemao3 = Pokemao(Types.WIND,PokemaoNames.MAOSAURO,0,0)
-# Pokemao4 = Pokemao(Types.DARKNESS,PokemaoNames.CURSEDHAND,0,0)
 
-# player2 = Player("player2", Pokemao1)
-# player2.pokemaos = Pokemao2
-# print(player2.name,player2.pokemaos.name.value)
 
 
 
