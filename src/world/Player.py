4fca4527989eaec863180cc11c98543272aa152a
Guilhermelaivi <guilhermelaivi@gmail.com>
2022-12-22 18:12:42 -0300

    the first option of the menu was created

--- src/world/Player.py
+++ src/world/Player.py
@@ -1,9 +1,34 @@
+import os
+
 class Player:
     age = 31 
     pokemaos = []
     def __init__(self,name,pokemaos):
         self.name = name
         self.pokemaos.append(pokemaos) 
+
+    def presentPokemaos(self):
+        os.system("cls||clear")
+        print("--- Pokemão List ---")
+        for pokemao in self.pokemaos:
+            print(pokemao)
+        input("Backspace to return") 
+        
+
+    def pokedex(self,optionUser):
+        while optionUser != 4:
+            match optionUser:
+                case "1":
+                    self.presentPokemaos()
+                case "2":
+                    print("it's time to duel")
+                case "3":
+                    print("it's time to capture pokemaos")
+                case _:
+                    os.system("cls||clear")
+                    print("End game!")
+                    break    
+        
 
     def insertPokemao(self,pokemao):
         print(len(self.pokemaos))
